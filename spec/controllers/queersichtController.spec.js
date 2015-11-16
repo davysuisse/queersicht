de1dc02be855a895b1d33dc545bcd8482cc322fe
davysuisse <davy_claude@hotmail.com>
2015-11-16 20:44:14 +0100

    Indent

--- spec/controllers/queersichtController.spec.js
+++ spec/controllers/queersichtController.spec.js
@@ -1,24 +1,27 @@
-(function(){
-	'use strict';
+(function() {
+  'use strict';
 
-    describe("Test Queersicht Controller", function () {
-      var   queersichtController,
-            $rootScope;
+  describe("Test Queersicht Controller", function() {
+    var queersichtController, $rootScope;
 
-      beforeEach(module('Queersicht'));
-      beforeEach(module('Queersicht.controllers'));
+    beforeEach(module('Queersicht'));
+    beforeEach(module('Queersicht.controllers'));
 
-      beforeEach(inject(function($controller, _$rootScope_){
-        $rootScope = _$rootScope_;
-        queersichtController = $controller('QueersichtController', { '$scope' : $rootScope.$new() });
-      }));
-
-      it("Test the broadcast to change the title", function () {
-        $rootScope.$broadcast('menu-title', { title : 'test' });
-
-        // When broadcasting, the navigation should have been affected
-        expect(queersichtController.navigation).toEqual('test');
+    beforeEach(inject(function($controller, _$rootScope_) {
+      $rootScope = _$rootScope_;
+      queersichtController = $controller('QueersichtController', {
+        '$scope': $rootScope.$new()
       });
+    }));
+
+    it("Test the broadcast to change the title", function() {
+      $rootScope.$broadcast('menu-title', {
+        title: 'test'
+      });
+
+      // When broadcasting, the navigation should have been affected
+      expect(queersichtController.navigation).toEqual('test');
     });
+  });
 
 })();
