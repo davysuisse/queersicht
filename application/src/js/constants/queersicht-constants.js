0eaaf52cff8076cb7d314e1747b9f239af451d0c
davysuisse <davy_claude@hotmail.com>
2015-12-08 05:54:15 +0100

    Images caching

--- application/src/js/constants/queersicht-constants.js
+++ application/src/js/constants/queersicht-constants.js
@@ -9,7 +9,7 @@
   function qsConstants() {
     return {
       // Server url
-      urlService : 'http://localhost:8081/api', //localhost 192.168.192.100
+      urlService : 'http://192.168.192.100:8081/api', //localhost 192.168.192.100
 
       defaultImage : 'images/default/queersicht.png',
       errorImage   : 'images/error/notfound.jpg',
